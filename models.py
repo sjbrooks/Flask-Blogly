e1ab7c68b7fbc55493ceaa1355542b89ce1a0c2a
Sarah <sjbrooks305@gmail.com>
2020-03-25 14:59:42 -0400

    added users model

--- models.py
+++ models.py
@@ -1,1 +1,27 @@
 """Models for Blogly."""
+
+from flask_alchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    '''User class.'''
+
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, 
+                   primary_key=True, 
+                   autoincrement=True)
+    first_name = db.Column(db.String(),
+                 nullable=False)
+    last_name = db.Column(db.String(),
+                 nullable=False)
+    img_URL = db.Column(db.String(),
+              nullable=True, default='https://picsum.photos/100
+')
+
