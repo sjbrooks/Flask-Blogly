5626ce5300a66216b1f51a64b4bbd7552c4bc1c5
Sarah <sjbrooks305@gmail.com>
2020-03-25 15:43:30 -0400

    added user-listing.html and base.html; also added a row to the users table and committed it to the db; added some additional imports to flask at top of app.py; created first app.py route for root

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, render_template, request
+from models import db, connect_db, User
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.conf['SECRET_KEY'] = 'zlxkcjvoiw34jpqr95ijafkdj'
 
 connect_db(app)
 db.create_all()
+
+debug = DebugToolbarExtension(app)
+
+@app.route('/users')
+def render_users():
+
+    users = User.query.all()
+    # # names = []
+
+    # # for user in users:
+    # #     names.append(User.combine_name())
+
+    # for user in users:
+
+    return render_template('user-listing.html', users=users)
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 """Models for Blogly."""
 
-from flask_alchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
@@ -8,6 +8,7 @@ def connect_db(app):
 
     db.app = app
     db.init_app(app)
+
 
 class User(db.Model):
     '''User class.'''
@@ -18,10 +19,19 @@ class User(db.Model):
                    primary_key=True, 
                    autoincrement=True)
     first_name = db.Column(db.String(),
-                 nullable=False)
+                           nullable=False)
     last_name = db.Column(db.String(),
-                 nullable=False)
+                          nullable=False)
     img_URL = db.Column(db.String(),
-              nullable=True, default='https://picsum.photos/100
-')
+                        nullable=True, 
+                        default='https://picsum.photos/100')
+
+    # def combine_name(self):
+    #     return f'{self.first_name} {self.last_name}'
+
+    def __repr__(self):
+    """Show info about user."""
+
+    u = self
+    return f"<User {u.id} {u.first_name} {u.last_name} {u.img_URL}>"
 
