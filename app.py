b77e892fc8b0a0df01283fc110bcf296a18b6f22
Andrei Kassiantchouk <akassian@berkeley.edu>
2020-03-25 16:44:12 -0700

    part1 edit form showing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -8,8 +8,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.conf['SECRET_KEY'] = 'zlxkcjvoiw34jpqr95ijafkdj'
+app.config['SECRET_KEY'] = 'zlxkcjvoiw34jpqr95ijafkdj'
 
+# db = SQLAlchemy()
 connect_db(app)
 db.create_all()
 
@@ -27,3 +28,33 @@ def render_users():
     # for user in users:
 
     return render_template('user-listing.html', users=users)
+
+@app.route('/users/new')
+def get_add_user():
+    return render_template('add-user.html')
+
+@app.route('/users/new', methods=["POST"])
+def post_add_user():
+    first_name = request.form.get("first-name")
+    last_name = request.form.get("last-name")
+    img_url = request.form.get("img-url")
+    print("\n \n \n IMG URL IS",img_url)
+    if(img_url == ""):
+        user = User(first_name=first_name, last_name=last_name)
+    else:
+        user = User(first_name=first_name, last_name=last_name, img_URL=img_url)
+    db.session.add(user)
+    db.session.commit()
+    return redirect('/users')
+
+@app.route('/users/<id>')
+def user_profile(id):
+    id = id
+    user = User.query.filter(User.id == id).first()
+    return render_template('user-profile.html', user=user)
+
+@app.route('/users/<id>/edit')
+def get_edit_user(id):
+    id = id
+    user = User.query.filter(User.id == id).first()
+    return render_template('edit-user.html', user=user)
--- models.py
+++ models.py
@@ -15,23 +15,24 @@ class User(db.Model):
 
     __tablename__ = 'users'
 
-    id = db.Column(db.Integer, 
-                   primary_key=True, 
+    id = db.Column(db.Integer,
+                   primary_key=True,
                    autoincrement=True)
     first_name = db.Column(db.String(),
                            nullable=False)
     last_name = db.Column(db.String(),
                           nullable=False)
     img_URL = db.Column(db.String(),
-                        nullable=True, 
+                        nullable=True,
                         default='https://picsum.photos/100')
 
     # def combine_name(self):
     #     return f'{self.first_name} {self.last_name}'
 
     def __repr__(self):
-    """Show info about user."""
+        """Show info about user."""
 
-    u = self
-    return f"<User {u.id} {u.first_name} {u.last_name} {u.img_URL}>"
+        u = self
+        return f"<User {u.id} {u.first_name} {u.last_name} {u.img_URL}>"
 
+# db.create_all()
